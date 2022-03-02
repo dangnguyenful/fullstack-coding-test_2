3494dc9dc36253152128fa2b22f3c75ba979b1ce
Anonymous <doannv@runsystem.net>
2022-03-02 19:08:11 +0700

    Add firebase

--- config/firebase.tsx
+++ config/firebase.tsx
@@ -1,8 +1,18 @@
+// export const firebaseConfig = {
+//     apiKey: "AIzaSyDmJws1IFMjcpkhU2UX8RsYW0CJJKBsbpQ",
+//     authDomain: "test-bf72f.firebaseapp.com",
+//     projectId: "test-bf72f",
+//     storageBucket: "test-bf72f.appspot.com",
+//     messagingSenderId: "1004910700622",
+//     appId: "1:1004910700622:web:be7d2016d4ec6dc8093b1f"
+// };
+
 export const firebaseConfig = {
-    apiKey: "AIzaSyDmJws1IFMjcpkhU2UX8RsYW0CJJKBsbpQ",
-    authDomain: "test-bf72f.firebaseapp.com",
-    projectId: "test-bf72f",
-    storageBucket: "test-bf72f.appspot.com",
-    messagingSenderId: "1004910700622",
-    appId: "1:1004910700622:web:be7d2016d4ec6dc8093b1f"
-};
+    apiKey: "AIzaSyAYolMUjv4xY0H4f_ZQLoytgOWH_lf41Go",
+    authDomain: "test-16ae4.firebaseapp.com",
+    projectId: "test-16ae4",
+    storageBucket: "test-16ae4.appspot.com",
+    messagingSenderId: "910157889750",
+    appId: "1:910157889750:web:cbe447b46fda8a298ec977",
+    measurementId: "G-XZQF6BQ9T6"
+}
